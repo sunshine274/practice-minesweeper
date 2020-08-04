54c6f2773ceed97384838c1103bb7cd9e326b42e
sunshine274 <jingx274@gmail.com>
2020-08-04 15:03:22 +0000

    Initiate the project

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+const grid = document.querySelector('.grid')
+let width = 10
+
+// Create board
+function createBoard() {
+    for(let i = 0; i < width*width ; i++){
+        
+    }
+}
+
+
+
+})
